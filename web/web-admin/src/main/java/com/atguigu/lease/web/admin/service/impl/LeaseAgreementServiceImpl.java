013d8cd83cb2655c7e89a6c2e307575b166973d3
enkou97 <enkou.97@gmail.com>
2026-04-15 14:52:08 +0900

    first commit

---
+++ web/web-admin/src/main/java/com/atguigu/lease/web/admin/service/impl/LeaseAgreementServiceImpl.java
@@ -0,0 +1,34 @@
+package com.atguigu.lease.web.admin.service.impl;
+
+import com.atguigu.lease.model.entity.LeaseAgreement;
+import com.atguigu.lease.web.admin.mapper.LeaseAgreementMapper;
+import com.atguigu.lease.web.admin.service.LeaseAgreementService;
+import com.atguigu.lease.web.admin.vo.agreement.AgreementQueryVo;
+import com.atguigu.lease.web.admin.vo.agreement.AgreementVo;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author liubo
+ * @description 针对表【lease_agreement(租约信息表)】的数据库操作Service实现
+ * @createDate 2023-07-24 15:48:00
+ */
+@Service
+public class LeaseAgreementServiceImpl extends ServiceImpl<LeaseAgreementMapper, LeaseAgreement>
+        implements LeaseAgreementService {
+
+    @Autowired
+    private LeaseAgreementMapper leaseAgreementMapper;
+
+    @Override
+    public IPage<AgreementVo> pageAgreement(Page<AgreementVo> page, AgreementQueryVo queryVo) {
+        return leaseAgreementMapper.pageAgreement(page, queryVo);
+    }
+}
+
+
+
+
